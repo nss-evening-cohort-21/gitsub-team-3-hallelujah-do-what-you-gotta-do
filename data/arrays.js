ae86c5b5fdcee1b8d14044570eacf85c9149c260
Eric Frey <ericlfrey@gmail.com>
2022-11-01 19:45:36 -0500

    Added Data Folder with and Arrays File. Imported Arrays into Main.JS

---
+++ data/arrays.js
@@ -0,0 +1,48 @@
+const reposArr = [
+  {
+    id: 1,
+    name: 'Sorting Hat',
+    pinned: true,
+    favorite: false,
+    description: 'Sorting Hat Project',
+    type: ['js', 'html', 'css']
+  },
+  {
+    id: 2,
+    name: 'Profile Page',
+    pinned: false,
+    favorite: true,
+    description: 'Profile Page Project',
+    type: ['html', 'css']
+  }
+];
+
+const projectsArr = [
+  {
+    id: 1,
+    name: 'Pet Adoption',
+    description: 'Pet Adoption Project',
+    dateAdded: ''
+  },
+  {
+    id: 2,
+    name: 'Binary Converter',
+    description: 'Binary Converter Project',
+    dateAdded: ''
+  }
+];
+
+const packagesArr = [
+  {
+    id: 1,
+    name: 'Apache',
+    description: 'Apache Package',
+    icon: ''
+  },
+  {
+    id: 2,
+    name: 'Docker',
+    description: 'Docker Package',
+    icon: ''
+  }
+];
