b1f3f064758d2c71c0709eebee9414ecb5f78cd0
Allison Blumenthal <blumen.allison@gmail.com>
2022-11-02 21:38:26 -0400

    changed projects cards to a list

--- scripts/main.js
+++ scripts/main.js
@@ -99,14 +99,32 @@ const projectsStringOnDom = () => {
   let projectsString = ``;
   for (const obj of projectsArr) {
     projectsString += `
-      <div class="card" style="width: 18rem;">
-        <Projects class="card-header">Projects Open-Closed</div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">${</li>
-          <li class="list-group-item">A second item</li>
-         <li class="list-group-item">A third item</li>           
-         </ul>
-      </div>`
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small>3 days ago</small>
+          </div>
+          <p class="mb-1">Some placeholder content in a paragraph.</p>
+          <small>And some small print.</small>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">List group item heading</h5>
+            <small class="text-muted">3 days ago</small>
+          </div>
+          <p class="mb-1">Some placeholder content in a paragraph.</p>
+      <small class="text-muted">And some muted small print.</small>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">List group item heading</h5>
+        <small class="text-muted">3 days ago</small>
+      </div>
+      <p class="mb-1">Some placeholder content in a paragraph.</p>
+      <small class="text-muted">And some muted small print.</small>
+    </a>
+  </div>
   }
 
 
