f2f7687aeb73b2bc55af3a0fdf2ac057e73b5cae
GrahamJReid <grahamreidtechnology@gmail.com>
2022-11-05 12:19:25 -0500

    overviewSearchBarAbsolute

--- components/overviewForm.js
+++ components/overviewForm.js
@@ -1,12 +1,12 @@
 export const overviewForm = `
 <div id="overviewForm">
-      <h3>Edit Pins</h3>
+      <h3 class="overview-sub-title">Edit Pins</h3>
  </div>
  <div class="input-group mb-3">
  <div class="input-group-prepend">
-      <button id="searchInput"  class="btn btn-outline-secondary" type="button">Search</button>
+    
  </div>
-     <input id="searchInput" type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+     <input id="searchInput" type="text" class="form-control" placeholder="Search Repositories" aria-label="" aria-describedby="basic-addon1">
 </div>
 <div class="form-control scroll" id="cardsForPin">
 </div>
@@ -19,3 +19,4 @@ export const overviewForm = `
  </div>
      <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
 </div>*/
+//<button id="searchInput"  class="btn btn-outline-secondary" type="button">Search</button>
