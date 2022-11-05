7d0f806985d4e0c5b0ffce6e0eab9c1000a8ab52
GrahamJReid <grahamreidtechnology@gmail.com>
2022-11-05 11:27:50 -0500

    overviewSearchBar Functional

--- components/overviewForm.js
+++ components/overviewForm.js
@@ -1,12 +1,17 @@
-export const overviewForm = `<form id="overviewForm">
-<div>
+export const overviewForm = `
+<div id="overviewForm">
       <h3>Edit Pins</h3>
  </div>
- 
+ <div class="input-group mb-3">
+ <div class="input-group-prepend">
+      <button id="searchInput"  class="btn btn-outline-secondary" type="button">Search</button>
+ </div>
+     <input id="searchInput" type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+</div>
 <div class="form-control scroll" id="cardsForPin">
 </div>
 </div>
-</form>`
+`
 
 /*<div class="input-group mb-3">
  <div class="input-group-prepend">
