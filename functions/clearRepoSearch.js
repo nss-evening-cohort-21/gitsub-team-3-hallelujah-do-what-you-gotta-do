33532bd4aca00dc30d4497150a7b505099b000f4
Eric Frey <ericlfrey@gmail.com>
2022-11-04 19:12:28 -0500

    Once again. Use This badboy

--- functions/clearRepoSearch.js
+++
@@ -1,9 +0,0 @@
-import { repoCardStrOnDom } from "./repoCardStrOnDom.js";
-import { reposArr } from "../data/arrays.js";
-
-export const clearRepoSearch = (e) => {
-  if (e.target.id === 'clearRepoSearch') {
-    document.querySelector('#repoSearch').value = '';
-    repoCardStrOnDom(reposArr);
-  }
-}
--- functions/langArrConstructor.js
+++
@@ -1,5 +0,0 @@
-export const langArrConstructor = (obj) => {
-  document.querySelector('#checkJs').checked ? obj.type.js = true : obj.type.js = false;
-  document.querySelector('#checkHtml').checked ? obj.type.html = true : obj.type.html = false;
-  document.querySelector('#checkCss').checked ? obj.type.css = true : obj.type.css = false;
-}
