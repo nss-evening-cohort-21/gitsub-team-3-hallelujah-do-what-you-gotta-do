9c592eb33a51eb1bee0c74b3d804e368e00cf70e
GrahamJReid <grahamreidtechnology@gmail.com>
2022-11-07 11:32:01 -0600

    overviewstyle1-5

--- components/overviewCardContainer.js
+++ components/overviewCardContainer.js
@@ -1,9 +1,8 @@
-export const overviewCardContainer = `<div id="overviewCardContainer">
-<div>
-   <h3 class="overview-sub-title">Pinned Repositories</h3>
-</div>
-
-<div  id="cardsPinned">  
-
-</div>
+export const overviewCardContainer =
+ `<div id="overviewCardContainer">
+   <div>
+      <h3 class="overview-sub-title">Pinned Repositories</h3>
+   </div>
+   <div id="cardsPinned">  
+   </div>
 </div>`
--- components/overviewForm.js
+++ components/overviewForm.js
@@ -1,22 +1,9 @@
 export const overviewForm = `
 <div id="overviewForm">
-      <h3 class="overview-sub-title">Edit Pins</h3>
- </div>
-
-    
- <div id="overviewSearchDiv">
-     <input id="searchInput" type="text" class="overview-search" placeholder="Search Repositories">
-</div>
-<div class=" scroll" id="cardsForPin">
-</div>
-</div>
-`
-
-/*<div class="input-group mb-3">
- <div class="input-group-prepend">
-      <button id="searchInput"  class="btn btn-outline-secondary" type="button">Search</button>
- </div>
-     <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
-</div>*/
-//<button id="searchInput"  class="btn btn-outline-secondary" type="button">Search</button>
-//aria-describedby="basic-addon1"
+     <h3 class="overview-sub-title">Edit Pins</h3>
+     <div id="overviewSearchDiv">
+          <input id="searchInput" type="text" class="overview-search" placeholder="Search Repositories">
+     </div>
+     <div class=" scroll" id="cardsForPin">
+     </div>
+</div>`
