728e3be335c5eb2c1cfdc6016f611bde9012f0ab
Allison Blumenthal <blumen.allison@gmail.com>
2022-11-07 21:34:22 -0500

    added sort functionality and styling

--- components/projectCardsDivOnDom.js
+++ components/projectCardsDivOnDom.js
@@ -2,10 +2,11 @@ export const projectsDivString = `
   <div class="input-group mb-3">
     <input type="text" class="form-control" placeholder="Search all projects" aria-describedby="basic-addon1" id="projectSearch">
     <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Sort</button>
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" >Sort</button>
       <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Project Name</a></li>
-        <li><a class="dropdown-item" href="#">Date Added</a></li>
+        <li><a class="dropdown-item" href="#" id="projectSortName">Project Name</a></li>
+        <li><a class="dropdown-item" href="#" id="projectSortDateNew">Newest to Oldest</a></li>
+        <li><a class="dropdown-item" href="#" id="projectSortDateOld">Oldest to Newest</a></li>
       </ul>
     </div>
   </div>
